bc7f7ff41bc6fc6b5b6c7b0bae4ec2cb42bc70f5
RuthMaria <ruth-15rms@hotmail.com>
2024-12-26 13:58:51 -0300

    refactor: remove as classes artistaDAL e musicaDAL e adiciona o método recuperarPor na classe generica DAL

--- ScreenSound/Banco/DAL.cs
+++ ScreenSound/Banco/DAL.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ScreenSound.Banco;
+﻿namespace ScreenSound.Banco;
 
 /*
+ 
+Diferença entre DAL (Data Access Object) e DAO (Data Access Layer)
+O DAL é a camada de acesso a dados que promove a abstração desses dados e vai 
+emitir todos os comandos de SELECT, INSERT, UPDATE E DELETE de maneira 
+separada da lógica das classes do projeto e independente da fonte de dados, 
+enquanto o DAO é um objeto do banco de dados que representa um banco aberto.
+
+Basicamente, o DAL representa a estrutura de acesso aos dados, independente do
+tipo de banco utilizado, e o DAO é o objeto que representa o acesso a uma 
+fonte de dados específica
+ 
+---------------------------------------------------------------------
+
  Uma classe abstrata é uma classe que serve como modelo ou base para 
  outras classes, mas não pode ser instanciada diretamente. 
  Em vez disso, ela é projetada para ser estendida por outras classes 
@@ -23,16 +30,15 @@ A interface é usada quando queremos apenas um contrato que será usado em
 várias classes não relacionadas entre sim, mas possuem um determinado
 comportamento semelhante. Interface não possui métodos concretos.
 
- */
+----------------------------------------------------------------------
 
-/*
  A parte 'where T : class' define uma restrição genérica. 
  Isso significa que o tipo T passado para a classe DAL<T> deve ser uma 
  classe (não pode ser um tipo primitivo como int, bool, etc.).
  Essa restrição é importante porque o código trabalha com entidades do 
  Entity Framework, que são classes.
  */
-internal abstract class DAL<T> where T : class
+internal class DAL<T> where T : class
 {
     protected readonly ScreenSoundContext context;
 
@@ -42,8 +48,8 @@ internal abstract class DAL<T> where T : class
     }
 
     /*
-       o Set identifica qual tipo estamos utilizando (artista ou música, por exemplo).
-       Em seguida, é feito o acesso ao conjunto de entidades do tipo T.
+     o Set identifica qual tipo estamos utilizando (artista ou música, por exemplo).
+     Em seguida, é feito o acesso ao conjunto de entidades do tipo T.
     */
     public IEnumerable<T> Listar()
     {
@@ -66,5 +72,17 @@ internal abstract class DAL<T> where T : class
     {
         context.Set<T>().Remove(objeto);
         context.SaveChanges();
+    }
+
+    /*
+     Func => desempenha o papel de uma função capaz de fornecer um retorno
+     e realizar uma verificação. É muito útil em situações em que
+     precisamos representar um método sem ter que passar especificamente
+     valores personalizados, deixando a correspondência para a assinatura 
+     do método.
+     */
+    public T? RecuperarPor(Func<T, bool> condicao)
+    {
+        return context.Set<T>().FirstOrDefault(condicao);
     }
 }
